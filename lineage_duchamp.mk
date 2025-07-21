31c88b3e694893387cd9e34adc09d3310c7194f4
Saikrishna1504 <saikrishna26918@gmail.com>
2025-07-21 18:54:22 +0530

    duchamp: Bringup @AxionAOSP
    Change-Id: If484064bb9615254ef5aba50f5b04430d68d9620
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- lineage_duchamp.mk
+++ lineage_duchamp.mk
@@ -14,6 +14,20 @@ $(call inherit-product, device/xiaomi/duchamp/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Axion Stuff
+AXION_CAMERA_REAR_INFO := 64,8,2
+AXION_CAMERA_FRONT_INFO := 16
+AXION_PROCESSOR := MTK_Dimensity_8300U
+AXION_MAINTAINER := Sai_Krishna
+TARGET_INCLUDES_LOS_PREBUILTS := true
+TARGET_INCLUDE_VIPERFX := true
+TARGET_ENABLE_BLUR := true
+
+# Axion CPU Flags
+AXION_CPU_SMALL_CORES := 0,1,2,3
+AXION_CPU_BIG_CORES := 4,5,6,7
+
+## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_duchamp
 PRODUCT_DEVICE := duchamp
 PRODUCT_MANUFACTURER := Xiaomi
@@ -27,4 +41,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="duchamp_global-user 15 AP3A.240905.015.A2 OS2.0.1.0.VNLMIXM release-keys" \
     BuildFingerprint=POCO/duchamp_global/duchamp:14/UP1A.230905.011/OS2.0.1.0.VNLMIXM:user/release-keys \
     DeviceProduct=$(PRODUCT_SYSTEM_NAME)
-
