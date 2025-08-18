9027f08b0c3cb5896298641022f0f2ddeca9d251
Mashopy <eliasgheeraert@gmail.com>
2025-08-18 05:59:08 +0200

    duchamp: Remove checkpoint_gc
    Google stopped using it since gs101, the exact reason why is unknown since it was done during early development.
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- device.mk
+++ device.mk
@@ -40,14 +40,7 @@ AB_OTA_POSTINSTALL_CONFIG += \
     FILESYSTEM_TYPE_system=$(TARGET_RO_FILE_SYSTEM_TYPE) \
     POSTINSTALL_OPTIONAL_system=true
 
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_vendor=true \
-    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
-    FILESYSTEM_TYPE_vendor=erofs \
-    POSTINSTALL_OPTIONAL_vendor=true
-
 PRODUCT_PACKAGES += \
-    checkpoint_gc \
     otapreopt_script
 
 PRODUCT_PACKAGES += \
