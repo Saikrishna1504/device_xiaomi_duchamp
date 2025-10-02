d9fd046242a1bce51ac7ff98d946ff9ec00b62a9
rio004 <riyon336@gmail.com>
2025-10-02 22:35:41 +0100

    duchamp: Update blobs to OS2.0.205.0.VNLMIXM
    Change-Id: I33431a64caddd22c7406e5e9eaaa83b30960a077
    Signed-off-by: rio004 <riyon336@gmail.com>
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- lineage_duchamp.mk
+++ lineage_duchamp.mk
@@ -35,5 +35,5 @@ PRODUCT_SYSTEM_NAME := duchamp_global
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildFingerprint=POCO/duchamp_global/duchamp:14/UP1A.230905.011/OS2.0.200.0.VNLMIXM:user/release-keys \
+    BuildFingerprint=POCO/duchamp_global/duchamp:14/UP1A.230905.011/OS2.0.205.0.VNLMIXM:user/release-keys \
     DeviceProduct=$(PRODUCT_SYSTEM_NAME)
