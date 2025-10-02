c4c6f8b3295bb6af0578524ee80cc0c926469d11
Saikrishna1504 <saikrishna26918@gmail.com>
2025-10-02 22:53:56 +0530

    duchamp: Enable torch light strength control
    Change-Id: I11a0993f910d746087db78bbeedb7ef35fccb61c
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- lineage_duchamp.mk
+++ lineage_duchamp.mk
@@ -22,6 +22,7 @@ AXION_MAINTAINER := Sai_Krishna
 TARGET_INCLUDES_LOS_PREBUILTS := true
 TARGET_INCLUDE_VIPERFX := true
 TARGET_ENABLE_BLUR := true
+TORCH_STR_SUPPORTED := true
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_duchamp
