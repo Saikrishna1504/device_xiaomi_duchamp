20ca3e9b5b26f7b5a8355e2d4d240316a54a03da
rio004 <riyon336@gmail.com>
2025-10-13 12:02:58 +0100

    Revert "duchamp: Enable Virtualization service"
    This reverts commit 95b71cdc0d7916e1a2b42bfccdf2b15d488bb8ff.
    
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- device.mk
+++ device.mk
@@ -380,9 +380,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vibratorfeature-wrapper
 
-# Virtualization service
-$(call inherit-product, packages/modules/Virtualization/apex/product_packages.mk)
-
 # Miui Camera Permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-miuicamera.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-miuicamera.xml
