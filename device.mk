2198b0dd66cc500f26cff573d47a4831d9a566df
Mashopy <eliasgheeraert@gmail.com>
2025-11-08 18:19:55 +0100

    duchamp: Build mediaTek WiFi lib and enable OUI support on wpa_supplicant_8
    Change-Id: I6461ffce9850b73b07c6f23fa590856c7d87b472
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- device.mk
+++ device.mk
@@ -351,6 +351,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/mediatek \
     hardware/mediatek/libmtkperf_client \
+    hardware/mediatek/wlan/wifi_hal \
     hardware/xiaomi
 
 # Thermal
@@ -385,10 +386,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/miuicamera-hiddenapi-package-allowlist.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/miuicamera-hiddenapi-package-allowlist.xml
 
 # Wi-Fi
+$(call soong_config_set,wpa_supplicant_8,board_wlan_mediatek_stability,true)
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
     hostapd \
-    libwifi-hal-wrapper \
     wpa_supplicant
 
 # Inherit the proprietary files
