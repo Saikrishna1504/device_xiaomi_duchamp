da7ea9703bf993375a058494126294d3c77d7e41
rio004 <riyon336@gmail.com>
2025-11-20 23:44:42 +0000

    duchamp: Switch to prebuilt audio impl
    Change-Id: I8fdcd476042bf84328748ccaded65a70335727f8
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- device.mk
+++ device.mk
@@ -69,7 +69,6 @@ TARGET_EXCLUDES_AUDIOFX := true
 
 PRODUCT_PACKAGES += \
     android.hardware.audio.service \
-    android.hardware.audio@7.1-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.soundtrigger@2.3-impl
 
