601ddcde56edd2a06c20668b30354fef075317b1
Saikrishna1504 <saikrishna26918@gmail.com>
2025-11-26 17:22:30 +0000

    Revert "duchamp: Build mediaTek WiFi lib and enable OUI support on wpa_supplicant_8"
    This reverts commit 2198b0dd66cc500f26cff573d47a4831d9a566df.

--- device.mk
+++ device.mk
@@ -350,7 +350,6 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/mediatek \
     hardware/mediatek/libmtkperf_client \
-    hardware/mediatek/wlan/wifi_hal \
     hardware/xiaomi
 
 
@@ -390,10 +389,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/miuicamera-hiddenapi-package-allowlist.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/miuicamera-hiddenapi-package-allowlist.xml
 
 # Wi-Fi
-$(call soong_config_set,wpa_supplicant_8,board_wlan_mediatek_stability,true)
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
     hostapd \
+    libwifi-hal-wrapper \
     wpa_supplicant
 
 # Inherit the proprietary files
