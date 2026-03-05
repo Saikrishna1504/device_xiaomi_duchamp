e4e36d571f3ea48f3f72dda6d13977dc7dd88a9b
rio004 <riyon336@gmail.com>
2026-03-05 23:32:21 +0000

    duchamp: Update blobs to OS2.0.209.0.VNLMIXM
    Change-Id: Ie53d7d1c569c6205ace16bba184eb45bdff22dd4
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- lineage_duchamp.mk
+++ lineage_duchamp.mk
@@ -38,5 +38,5 @@ PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildFingerprint=POCO/duchamp_global/duchamp:14/UP1A.230905.011/OS2.0.205.0.VNLMIXM:user/release-keys \
+    BuildFingerprint=POCO/duchamp_global/duchamp:14/UP1A.230905.011/OS2.0.209.0.VNLMIXM:user/release-keys \
     DeviceProduct=$(PRODUCT_SYSTEM_NAME)
